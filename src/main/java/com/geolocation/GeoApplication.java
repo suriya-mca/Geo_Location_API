00da6cc86be46f0adda354f49ddfda8cce0e4219
Suriya <suriyag1999@gmail.com>
2024-03-24 17:28:32 +0530

    first commit

---
+++ src/main/java/com/geolocation/GeoApplication.java
@@ -0,0 +1,13 @@
+package com.geolocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoApplication.class, args);
+	}
+
+}
